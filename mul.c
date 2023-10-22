c83f9dda5b7a24762ed9090d0f6271d442a6e052
jomojay <djdrjay001@gmail.com>
2023-10-22 01:59:51 +0100

    quick fix

--- mul.c
+++ mul.c
@@ -8,7 +8,7 @@
  * from the second top element of the stack.
  * @stack: pointer to head of stack
  * @line_number: line number for current instruction
- * @arg: argument, if any
+ * @arg: argument if any
  *
  * Return: 0 if successful, -1 on failure
  */
